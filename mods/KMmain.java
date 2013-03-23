6e1d03188c48f1993c05abf3c0ead81bd51a91ae
Keksynator <keksynator@web.de>
2013-03-23 22:03:11 +0100

    void load Init

--- mods/KMmain.java
+++ mods/KMmain.java
@@ -1,11 +1,19 @@
 package km.mods;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "Keksynator_KMmod", name = "KMmod", version = "PreA1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)
 
 public class KMmain {
+	
+	@Init
+	public void load(FMLInitializationEvent event)
+	{
+		
+	}
 
 }
